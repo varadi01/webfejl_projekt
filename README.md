4296ea3aae7996a0ee9e0f245abede288c4beb56
Máté Váradi <varadi.mate01@gmail.com>
2024-10-30 17:22:06 +0100

    chores, updated README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # reddit-clone
 
-This project is a Reddit clone made with Java Spring and .
+This project is a Reddit clone made with Java Spring Boot and React.js.
 
 The project is my naive go at creating a reddit lookalike, aptly named "rabbit". I only focus on essential features you would normally find on a forum-style platform such as Reddit; so no embedded content or streaming and stuff.
 
@@ -8,12 +8,14 @@ The project is my naive go at creating a reddit lookalike, aptly named "rabbit".
  - Docker Desktop or Docker Compose
  - Maven
  - JDK 21 or later
+ - Node?
 
 
 ## Building the project
 Clone the Github repository
  - run: ```docker compose up -d```
  - run: ```mvn clean spring-boot:run```
+//TODO
 
 You should be able to access the application at localhost:8080.
 
--- src/main/java/hu/unideb/inf/redditclone/RedditcloneApplication.java
+++ src/main/java/hu/unideb/inf/redditclone/RedditcloneApplication.java
@@ -1,7 +1,18 @@
 package hu.unideb.inf.redditclone;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import jakarta.servlet.Servlet;
+import jakarta.faces.webapp.FacesServlet;
+import jakarta.servlet.DispatcherType;
+import java.util.EnumSet;
 
 @SpringBootApplication
 public class RedditcloneApplication {
@@ -9,5 +20,28 @@ public class RedditcloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RedditcloneApplication.class, args);
 	}
+
+
+	/*
+	@Bean
+	public ServletRegistrationBean<Servlet> facesServletRegistration() {
+		//I MODIFIED THIS, IT MIGHT NOT WORK eg. new faceservlet
+		ServletRegistrationBean<Servlet> registration = new ServletRegistrationBean<Servlet>(new FacesServlet(), "*.jsf");
+		return registration;
+	}
+
+	 */
+
+	/*
+	@Bean
+	public FilterRegistrationBean<RewriteFilter> rewriteFilter() {
+		//MIGHT WANT TO USE OLD REWRITE
+		FilterRegistrationBean<RewriteFilter> rwFilter = new FilterRegistrationBean<RewriteFilter>(new RewriteFilter());
+		rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+				DispatcherType.ASYNC, DispatcherType.ERROR));
+		rwFilter.addUrlPatterns("/*");
+		return rwFilter;
+	}
+	 */
 
 }
